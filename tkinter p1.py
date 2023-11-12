b846f1c7b3282dbe405812341317c142bba31514
MayureshD91 <139664373+MayureshD91@users.noreply.github.com>
2023-11-12 18:35:26 +0530

    Tkinter first program

---
+++ tkinter p1.py
@@ -0,0 +1,52 @@
+#tkinter
+
+import tkinter as tk
+
+windows = tk.Tk()
+windows.geometry("500x500")
+windows.minsize(200,200)
+windows.maxsize(700,700)
+windows.title("This is my first TKinter program")
+windows.resizable(False, False)
+windows.mainloop()
+
+import tkinter as tk
+
+windows = tk.Tk()
+windows.geometry("500x500")
+windows.minsize(200,200)
+windows.maxsize(700,700)
+windows.title("This is my First Tkinter Program")
+windows.resizable(False, False)
+
+windows.mainloop()
+
+
+import tkinter as tk
+
+def red():
+    label1.config(bg="red", font=("Elephant","18","bold"), text="RED BUTTON IS PRESSED")
+    button1.config(bg="red")
+
+def green():
+    label1.config(bg="green", font=("Elephant","18","bold"), text="GREEN BUTTON IS PRESSED")
+    button2.config(bg="green")
+
+def blue():
+    label1.config(bg="blue", font=("Elephant","18","bold"), text="BLUE BUTTON IS PRESSED")
+    button3.config(bg="blue")
+
+
+
+windows = tk.Tk()
+windows.geometry("600x300")
+
+
+label1=tk.Label(windows,text="pls change my color",bg="yellow")
+button1=tk.Button(windows, fg="#1e81b0", text="RED", command=red, activebackground="red", height=2, width=40, font=("elephant", 18))
+button2=tk.Button(windows, fg="#EAA408", text="GREEN", command=green, activebackground="green", activeforeground= "red", height=2, width=40, font=("Arial", 18))
+button3=tk.Button(windows, fg="#EA408",text="BLUE", command=blue, activebackground="blue", activeforeground="red", height=2, width=40, font=("Consolas",18))
+label1.pack()
+button1.pack()
+button2.pack()
+button3.pack()
